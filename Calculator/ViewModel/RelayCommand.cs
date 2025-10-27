8b01dedfc60ea2b70283c32e247052f99e48b2b9
Jay <iamjyh99@gmail.com>
2025-10-27 17:56:14 +0900

    Add RelayCommand class implementing ICommand interface

---
+++ Calculator/ViewModel/RelayCommand.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calculator.ViewModel
+{
+    class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        // 파라미터가 있는 메서드를 위한 생성자
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        // 파라미터가 없는 메서드를 위한 생성자
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = p => execute();
+            _canExecute = canExecute != null ? p => canExecute() : null;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
